9d4a350c51d42646057cb752c723c6477c9fdd27
Julian Peters <julian.l.peters@gmail.com>
2017-01-23 21:02:15 +0100

    Cleaned up code, converted names/intend/lines to PEP 8
    Changed agent stocks to dictionary.
    Added __repr__ for classes
    Added 1 for 1 stock distribution to simulation

--- firms.py
+++ firms.py
@@ -1,38 +1,46 @@
 """In this file, we define the benchmark stock market model firm classes"""
-__author__ = 'Schasfoort, Abeshzadeh, Broek & Peters'
 
 import random
-import stocks
 
-class Firm():
-	"""a base class for Firms"""
-	
-	def __init__(self, id ,bookValue, profit, profitHistory, seed, dividendRate = 1):
-		"""Creates a new trader"""
-		self.id = id
-		self.bookValue = bookValue
-		self.profit = profit
-		self.dividendRate = dividendRate
-		self.profitHistory = profitHistory
-		self.profitHistory.append(profit)
-		self.profitGrowthHistory = [(j/float(i))-1 for i, j in zip(self.profitHistory[:-1], self.profitHistory[1:])]
-		random.seed(seed)
-		
-	def update_profits(self):
-		# TODO update 
-		growth = 95 + random.randint(0,10)
-		self.profit = int((self.profit * growth) / 100)
-		self.profitGrowthHistory.append((growth - 100) / 100)
-		self.profitHistory.append(self.profit)
-        
-def testMethod():
-	testFirm = Firm(1, 1000, 10000, [10000, 10000], seed = 1)
-	print(testFirm.profitHistory)
-	print(testFirm.profitGrowthHistory)
-	print(testFirm.profit)
-	testFirm.update_profits()
-	print(testFirm.profit)
-	print(testFirm.profitHistory)
-	print(testFirm.profitGrowthHistory)
-	
-#testMethod()
+__author__ = 'Schasfoort, Abeshzadeh, Broek & Peters'
+
+
+class Firm:
+    """a base class for Firms"""
+
+    def __init__(self, name, book_value, profit, profit_history, seed, dividend_rate=1):
+        """Creates a new trader"""
+        self.name = name
+        self.book_value = book_value
+        self.profit = profit
+        self.dividend_rate = dividend_rate
+        self.profit_history = profit_history
+        self.profit_history.append(profit)
+        self.profit_growth_history = [(j / float(i)) - 1 for i, j in zip(self.profit_history[:-1],
+                                                                         self.profit_history[1:])]
+        random.seed(seed)
+
+    def update_profits(self):
+        # TODO update
+        growth = 95 + random.randint(0, 10)
+        self.profit = int((self.profit * growth) / 100)
+        self.profit_growth_history.append((growth - 100) / 100)
+        self.profit_history.append(self.profit)
+
+    def __repr__(self):
+        return str(self.name)
+
+    def __str__(self):
+        return str(self.name)
+
+
+def test_method():
+    test_firm = Firm(12, 1000, 10000, [10000, 10000], seed=1)
+    print(test_firm.profit_history)
+    print(test_firm.profit_growth_history)
+    print(test_firm.profit)
+    test_firm.update_profits()
+    print(test_firm.profit)
+    print(test_firm.profit_history)
+    print(test_firm.profit_growth_history)
+    print(test_firm)
--- setup.py
+++ setup.py
@@ -1,42 +1,47 @@
 """In this file, we define the set-up procedure of the model"""
-__author__ = 'Schasfoort, Abeshzadeh, Broek & Peters'
 
 import agents
 import random
 import firms
 import functions
 
-def setupAgents(initMoney, initBidAskSpread, initMemorySize,seed ,amountOfAgents=3):
-	"""This returns an initialized agent set"""
-	random.seed(seed)
-	agentset = []
-	for agent in range(amountOfAgents):
-		agentset.append(agents.Trader(id=agent, 
-								money=randomizeInitVariable(initMoney[0], initMoney[1]), 
-								bidAskSpread=randomizeInitVariable(initBidAskSpread[0], initBidAskSpread[1]), 
-								memorySize=randomizeInitVariable(initMemorySize[0], initMemorySize[1]))) 
-	return agentset
+__author__ = 'Schasfoort, Abeshzadeh, Broek & Peters'
 
-def setupFirms(initBookValue, initProfit, initprofitHistory, seed, amountOfFirms=1):
+
+def setup_agents(init_money, init_bid_ask_spread, init_memory_size, seed, amount_of_agents=3):
+    """This returns an initialized agent set"""
+    random.seed(seed)
+    agent_set = []
+    for agent in range(amount_of_agents):
+        agent_set.append(agents.Trader(name=agent,
+                                       money=randomize_init_variable(init_money[0], init_money[1]),
+                                       bid_ask_spread=randomize_init_variable(init_bid_ask_spread[0], init_bid_ask_spread[1]),
+                                       memory_size=randomize_init_variable(init_memory_size[0], init_memory_size[1])))
+    return agent_set
+
+
+def setup_firms(init_book_value, init_profit, init_profit_history, seed, amount_of_firms=1):
     """This returns an initialized firm set"""
     random.seed(seed)
-    firmSet = []
-    for firm in range(amountOfFirms):
-        firmSet.append(firms.Firm(id=firm ,bookValue=randomizeInitVariable(initBookValue[0], initBookValue[1]),
-                            profit=randomizeInitVariable(initProfit[0], initProfit[1]),
-                            profitHistory = initprofitHistory, 
-                            # this makes sure that individual firms have a different seed every simulation
-                            seed=random.randint(0,10000), 
-                            dividendRate = 1))
-    return firmSet
- 
-def setupStocks(setOfFirms, faceValue):
-    stockSet = []
-    for firm in setOfFirms:
-        stockSet.append(functions.create_stocks(firm, faceValue))
-    return stockSet
- 
-def randomizeInitVariable(minamount, maxamount):
-	amount = random.randint(minamount, maxamount)
-	return amount
-	
+    firm_set = []
+    for firm in range(amount_of_firms):
+        firm_set.append(firms.Firm(name=firm,
+                                   book_value=randomize_init_variable(init_book_value[0], init_book_value[1]),
+                                   profit=randomize_init_variable(init_profit[0], init_profit[1]),
+                                   profit_history=init_profit_history,
+                                   # this makes sure that individual firms have a different seed every simulation
+                                   seed=random.randint(0, 10000),
+                                   dividend_rate=1))
+    return firm_set
+
+
+def setup_stocks(set_of_firms, face_value):
+    stock_set = []
+    for firm in set_of_firms:
+        stock_set.append(functions.create_stocks(firm, face_value))
+    return stock_set
+
+
+def randomize_init_variable(min_amount, max_amount):
+    return random.randint(min_amount, max_amount)
+
