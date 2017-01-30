99327139d5c72912011f34e747c28d15762944d7
Mark <markvanderbroek@gmail.com>
2017-01-30 10:08:24 +0100

    Seperated the function for selecting the observable traders set from the market mechanism

--- marketmechanisms.py
+++ marketmechanisms.py
@@ -5,34 +5,51 @@ __author__ = 'Schasfoort, Abeshzadeh, Broek & Peters'
 import random
 import math
 
-def bounded_best_price(agentset, observablesetsize, stock, valuation_function, seed):
+
+def market_mechanism(agentset, observablesetsize, stock, valuation_function, set_of_traders_function, seed):
     """return set of matched agents"""
-    setOfMatchedAgents = []
+    set_of_matched_agents = []
     random.seed(seed)
-    randomizedAgentSet = list(agentset)
-    random.shuffle(randomizedAgentSet)
+
+    # copy the agentset and shuffle the set to get different order of traders every time
+    randomized_agent_set = list(agentset)
+    random.shuffle(randomized_agent_set)
 
     # every demand agent observes a subset of other agents from which it picks the cheapest supplier
-    for demander in randomizedAgentSet:
-        demanderPrice = demander.valuate_stocks(stock=stock, valuation_function=valuation_function) \
-                        * (1-(demander.bid_ask_spread/200)) # /2 and / 100 for percent
-        totalObservableSet = list(randomizedAgentSet)
-        totalObservableSet.remove(demander)
-        observableSet = [totalObservableSet[i] for i in sorted(random.sample(range(len(totalObservableSet)),
-                                                                             observablesetsize))]
+    for demander in randomized_agent_set:
+        demander_price = demander.valuate_stocks(stock=stock, valuation_function=valuation_function) \
+                         * (1 - (demander.bid_ask_spread / 200))  # /2 and / 100 for percent
+
+        # find the observable set for the demanding agent
+        observable_set = set_of_traders_function(demander, randomized_agent_set, observablesetsize)
+
         # Find cheapest supplier and safe the price
-        minSupplierandPrice = (None, math.inf)
+        best_supplier_and_price = (None, math.inf)
 
-        for supplier in observableSet:
-            supplierPrice = supplier.valuate_stocks(stock=stock, valuation_function=valuation_function) \
-            * (1 + (supplier.bid_ask_spread / 200))
+        for supplier in observable_set:
+            supplier_price = supplier.valuate_stocks(stock=stock, valuation_function=valuation_function) \
+                             * (1 + (supplier.bid_ask_spread / 200))
 
-            if supplierPrice < minSupplierandPrice[1]:
-                minSupplierandPrice = (supplier, supplierPrice)
+            if suitable_trade(demander, supplier, best_supplier_and_price, stock):
+                best_supplier_and_price = (supplier, supplier_price)
 
         # If the minimum price of all suppliers is lower than the ask price, make deal with demander
-        if minSupplierandPrice[1] <= demanderPrice:
-            setOfMatchedAgents.append((demander, minSupplierandPrice[0]))
+        set_of_matched_agents.append((demander, best_supplier_and_price[0]))
 
-    return setOfMatchedAgents
+    return set_of_matched_agents
     # observe random set and take highest
+
+
+def suitable_trade(demander, supplier, best_supplier_and_price, stock):
+    demander_price = demander.valuate_stocks(stock=stock, valuation_function=valuation_function) \
+                     * (1 - (demander.bid_ask_spread / 200))  # /2 and / 100 for percent
+    supplier_price = supplier.valuate_stocks(stock=stock, valuation_function=valuation_function) \
+                     * (1 + (supplier.bid_ask_spread / 200))
+
+    # If the demander price is lower than what the supplier wants for the stock, no trade is possible
+    if demander_price < supplier_price:
+        return False
+
+    # A trade is only possible if the supplier price is the lowest, and there's stock available.
+    if supplier_price < best_supplier_and_price[1] and supplier.stocks["1"] > 0:
+        return True
