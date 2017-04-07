87086b9d539bb5902c76f7514b74e158ddc04e57
Julian Peters <julian.l.peters@gmail.com>
2017-04-07 15:55:01 +0200

    started test function for agent

--- stockmarket/valuationfunctions.py
+++ stockmarket/valuationfunctions.py
@@ -14,10 +14,11 @@ def valuation_extrapolate_average(stock, memory, **_):
 
 
 def valuation_extrapolate_growth_average(stock, memory, **_):
-    profit_growth_history = stock.firm.profit_growth_history
-    expected_growth = np.mean(profit_growth_history[len(profit_growth_history)-memory:len(profit_growth_history)])
-    value = calculate_npv(stock.firm.profit * stock.firm.dividend_rate, growth_rate=expected_growth)
-    return np.divide(value, stock.amount)
+    pass
+    # profit_growth_history = stock.firm.profit_growth_history
+    # expected_growth = np.mean(profit_growth_history[len(profit_growth_history)-memory:len(profit_growth_history)])
+    # value = calculate_npv(stock.firm.profit * stock.firm.dividend_rate, growth_rate=expected_growth)
+    #  return np.divide(value, stock.amount)
 
 
 def price_trend(stock, memory, **_):
