d25221d7498fc34a832c99f215fdf0a43e5a69de
Julian Peters <julian.l.peters@gmail.com>
2017-04-26 01:32:47 +0200

    test

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='abm',
+      version='0.1',
+      description='an agent based model to simulate the stock market',
+      url='http://github.com/LCfP/abm',
+      author='',
+      author_email='',
+      license='MIT',
+      packages=['stockmarket'])
