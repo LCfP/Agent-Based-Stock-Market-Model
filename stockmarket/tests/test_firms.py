504fb1b43bca49136e9ce97df0931f32073e09c8
Julian Peters <julian.l.peters@gmail.com>
2017-04-27 23:18:44 +0200

    Testing functions and valuation functions

--- stockmarket/tests/test_firms.py
+++ stockmarket/tests/test_firms.py
@@ -1,10 +1,8 @@
 """This file runs tests for all functions in firms"""
 
-import pytest
 from stockmarket.firms import Firm
-from stockmarket.stocks import Stock
 from numpy.testing import assert_equal
-from numpy.testing import assert_raises
+
 
 def test_update_profit():
     firm = Firm("test", 10000, [100], 1)
