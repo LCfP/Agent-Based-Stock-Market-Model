4fa3db0c3f512def99441549fe016718dc3c637a
joerischasfoort <joerischasfoort@gmail.com>
2017-07-28 12:59:51 +0200

    updatedOrderBookTests

--- stockmarket/tests/test_limitorderbook.py
+++ stockmarket/tests/test_limitorderbook.py
@@ -22,95 +22,91 @@ def limitorderbooks():
     return [LimitOrderBook(stocks, 100, 120)]
 
 def test_add_bid():
-    order_book = limitorderbooks[0]
-    order_book.add_bid(10, 20, 'trader-1')
-    order_book.add_bid(5, 20, 'trader-2')
-    order_book.add_bid(7, 20, 'trader-3')
-    order_book.add_bid(7, 20, 'trader-4')
-    order_book.add_bid(7, 20, 'trader-5')
+    limitorderbooks[0].add_bid(10, 20, 'trader-1')
+    limitorderbooks[0].add_bid(5, 20, 'trader-2')
+    limitorderbooks[0].add_bid(7, 20, 'trader-3')
+    limitorderbooks[0].add_bid(7, 20, 'trader-4')
+    limitorderbooks[0].add_bid(7, 20, 'trader-5')
     # highest bid is 10
-    assert_equal(order_book.bids[-1].price, 10)
+    assert_equal(limitorderbooks[0].bids[-1].price, 10)
     # lowest bid is 5
-    assert_equal(order_book.bids[0].price, 5)
+    assert_equal(limitorderbooks[0].bids[0].price, 5)
     # second highest bid trader = trader-2
-    assert_equal(order_book.bids[-2].owner, 'trader-3')
+    assert_equal(limitorderbooks[0].bids[-2].owner, 'trader-3')
 
 def test_add_ask():
-    order_book = limitorderbooks[0]
-    order_book.add_ask(11, 20, 'trader-1')
-    order_book.add_ask(5, 20, 'trader-2')
-    order_book.add_ask(7, 20, 'trader-3')
-    order_book.add_ask(7, 20, 'trader-4')
-    order_book.add_ask(7, 20, 'trader-5')
+    limitorderbooks[0].add_ask(11, 20, 'trader-1')
+    limitorderbooks[0].add_ask(5, 20, 'trader-2')
+    limitorderbooks[0].add_ask(7, 20, 'trader-3')
+    limitorderbooks[0].add_ask(7, 20, 'trader-4')
+    limitorderbooks[0].add_ask(7, 20, 'trader-5')
     # highest ask is 11
-    assert_equal(order_book.asks[-1].price, 11)
+    assert_equal(limitorderbooks[0].asks[-1].price, 11)
     # lowest ask is 5
-    assert_equal(order_book.asks[0].price, 5)
+    assert_equal(limitorderbooks[0].asks[0].price, 5)
     # second highest ask trader = trader-5
-    assert_equal(order_book.asks[-2].owner, 'trader-5')
+    assert_equal(limitorderbooks[0].asks[-2].owner, 'trader-5')
 
 def test_clean_book():
-    order_book = limitorderbooks[0]
-    order_book.add_bid(10, 20, 'trader-1')
-    order_book.add_bid(5, 20, 'trader-2')
-    order_book.add_ask(11, 20, 'trader-1')
-    order_book.add_ask(5, 20, 'trader-2')
+    limitorderbooks[0].add_bid(10, 20, 'trader-1')
+    limitorderbooks[0].add_bid(5, 20, 'trader-2')
+    limitorderbooks[0].add_ask(11, 20, 'trader-1')
+    limitorderbooks[0].add_ask(5, 20, 'trader-2')
     for n in range(119):
-        order_book.clean_book()
-    assert_equal(len(order_book.bids), 2)
-    assert_equal(len(order_book.asks), 2)
-    order_book.clean_book()
-    assert_equal(len(order_book.bids), 0)
-    assert_equal(len(order_book.asks), 0)
+        limitorderbooks[0].clean_book()
+    assert_equal(len(limitorderbooks[0].bids), 2)
+    assert_equal(len(limitorderbooks[0].asks), 2)
+    limitorderbooks[0].clean_book()
+    assert_equal(len(limitorderbooks[0].bids), 0)
+    assert_equal(len(limitorderbooks[0].asks), 0)
 
 def test_match_orders():
-    order_book = limitorderbooks[0]
     # add some asks
-    order_book.add_ask(5, 20, 'trader-2')
-    order_book.add_ask(7, 20, 'trader-3')
-    order_book.add_ask(7, 20, 'trader-4')
-    order_book.add_ask(7, 20, 'trader-5')
+    limitorderbooks[0].add_ask(5, 20, 'trader-2')
+    limitorderbooks[0].add_ask(7, 20, 'trader-3')
+    limitorderbooks[0].add_ask(7, 20, 'trader-4')
+    limitorderbooks[0].add_ask(7, 20, 'trader-5')
     # and bids
-    order_book.add_bid(10, 20, 'trader-1')
-    order_book.add_bid(4, 20, 'trader-11')
-    order_book.add_bid(9, 20, 'trader-11')
-    matched_orders = order_book.match_orders()
+    limitorderbooks[0].add_bid(10, 20, 'trader-1')
+    limitorderbooks[0].add_bid(4, 20, 'trader-11')
+    limitorderbooks[0].add_bid(9, 20, 'trader-11')
+    matched_orders = limitorderbooks[0].match_orders()
     # after an orderbook match both order books are reduced by 1
-    assert_equal(len(order_book.bids), 2)
-    assert_equal(len(order_book.asks), 3)
+    assert_equal(len(limitorderbooks[0].bids), 2)
+    assert_equal(len(limitorderbooks[0].asks), 3)
     # and the highest bid was matched to the lowest ask, difference is 5
     assert_equal(matched_orders[2].price - matched_orders[3].price, 5)
     # the second match is bid p9 and ask p7, difference is 2
-    matched_orders = order_book.match_orders()
+    matched_orders = limitorderbooks[0].match_orders()
     assert_equal(matched_orders[2].price - matched_orders[3].price, 2)
     # Once again the order books are reduced by 1 in size
-    assert_equal(len(order_book.bids), 1)
-    assert_equal(len(order_book.asks), 2)
+    assert_equal(len(limitorderbooks[0].bids), 1)
+    assert_equal(len(limitorderbooks[0].asks), 2)
     # then no more match is possible
-    matched_orders = order_book.match_orders()
+    matched_orders = limitorderbooks[0].match_orders()
     assert_equal(matched_orders, None)
     for n in range(500):
-        order_book.clean_book()
-    order_book.add_ask(5, 10, 'trader-2')
-    order_book.add_ask(7, 8, 'trader-3')
-    order_book.add_bid(10, 20, 'trader-1')
+        limitorderbooks[0].clean_book()
+    limitorderbooks[0].add_ask(5, 10, 'trader-2')
+    limitorderbooks[0].add_ask(7, 8, 'trader-3')
+    limitorderbooks[0].add_bid(10, 20, 'trader-1')
     # first match should reduce asks book by 1
-    matched_orders = order_book.match_orders()
-    assert_equal(len(order_book.bids), 1)
-    assert_equal(len(order_book.asks), 1)
+    matched_orders = limitorderbooks[0].match_orders()
+    assert_equal(len(limitorderbooks[0].bids), 1)
+    assert_equal(len(limitorderbooks[0].asks), 1)
     assert_equal(matched_orders[2].price - matched_orders[3].price, 5)
     # the bid should have a remaining volume of 10
-    assert_equal(order_book.bids[0].volume, 10)
+    assert_equal(limitorderbooks[0].bids[0].volume, 10)
     # second match
-    matched_orders = order_book.match_orders()
+    matched_orders = limitorderbooks[0].match_orders()
     # should reduce the lenght of the asks book to zero and bids should remain 1
-    assert_equal(len(order_book.bids), 1)
-    assert_equal(len(order_book.asks), 0)
+    assert_equal(len(limitorderbooks[0].bids), 1)
+    assert_equal(len(limitorderbooks[0].asks), 0)
     # the bid should have a remaining volume of 2
-    assert_equal(order_book.bids[0].volume, 2)
+    assert_equal(limitorderbooks[0].bids[0].volume, 2)
     # price should be 7 and volume 8
     assert_equal(matched_orders[0], 7)
     assert_equal(matched_orders[1], 8)
     # no more matches should be possible, leaving the order in the orderbook
-    assert_equal(order_book.match_orders(), None)
+    assert_equal(limitorderbooks[0].match_orders(), None)
 
